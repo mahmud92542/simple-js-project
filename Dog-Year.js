c71ad090e08825c7803fbd62030f09cea9458a7d
Mohammad Mahmud Hasan <34007876+mahmud92542@users.noreply.github.com>
2020-05-23 01:19:41 +0600

    Create Dog-Year.js

---
+++ Dog-Year.js
@@ -0,0 +1,13 @@
+var myAge = 23
+//Create a variable named myAge, and set it equal to your age as a number.
+let earlyYears = 2
+//Create a variable named earlyYears and save the value 2 to it. Note, the value saved to this variable will change.
+earlyYears = earlyYears * 10.5
+let laterYears = myAge - 2
+laterYears *= 4
+console.log(earlyYears)
+console.log(laterYears)
+var myAgeInDogYears = earlyYears + laterYears
+//Add earlyYears and laterYears together, and store that in a variable named myAgeInDogYears.
+var myName = 'MAHMUD'.toLowerCase()
+console.log (`My name is ${myName}. I am ${myAge} years old in human years which is ${myAgeInDogYears} years old in dog years.`)
